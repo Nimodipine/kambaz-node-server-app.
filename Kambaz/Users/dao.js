60b3e0e3970cb63da1e011baa46734d051c5107d
Nimodipine <megan5775@gmail.com>
2025-08-07 22:52:44 -0700

    added modification to match  model and schema

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,7 +1,9 @@
 import model from "./model.js";
 import { v4 as uuidv4 } from "uuid";
-let { users } = db;
+
 export const createUser = (user) => {
+    const newUser = { ...user, _id: uuidv4() };
+    return model.create(newUser);
 };
 export const findAllUsers = () => model.find();
 export const findUserById = (userId) => model.findById(userId);
@@ -9,3 +11,11 @@ export const findUserByUsername = (username) => model.findOne({ username: userna
 export const findUserByCredentials = (username, password) => model.findOne({ username, password });
 export const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+export const findUsersByRole = (role) => model.find({ role: role });
+export const findUsersByPartialName = (partialName) => {
+    const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+    return model.find({
+        $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+    });
+};
+
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
 import session from "express-session";
 import "dotenv/config";
@@ -15,6 +16,8 @@ import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
 
+const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 
 // Middleware
