3c6e98aea55add020b2cf28872185fff5205c6df
Nimodipine <megan5775@gmail.com>
2025-08-08 11:12:31 -0700

    modified coursed to connect to database

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -2,31 +2,37 @@ import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
 
 export default function CourseRoutes(app) {
-    app.get("/api/courses", (req, res) => {
-        const courses = dao.findAllCourses();
+    const findAllCourses = async (req, res) => {
+        const courses = await dao.findAllCourses();
         res.send(courses);
-    });
+    };
 
-    app.delete("/api/courses/:courseId", (req, res) => {
+    const createCourse = async (req, res) => {
+        const course = await dao.createCourse(req.body);
+        res.json(course);
+    };
+
+
+    const deleteCourse = async (req, res) => {
         const { courseId } = req.params;
-        const status = dao.deleteCourse(courseId);
+        const status = await dao.deleteCourse(courseId);
         res.send(status);
-    });
+    };
 
-    app.put("/api/courses/:courseId", (req, res) => {
+    const updateCourse = async (req, res) => {
         const { courseId } = req.params;
         const courseUpdates = req.body;
-        const status = dao.updateCourse(courseId, courseUpdates);
+        const status = await dao.updateCourse(courseId, courseUpdates);
         res.send(status);
-    });
+    };
 
-    app.get("/api/courses/:courseId/modules", (req, res) => {
+    const findModulesForCourse = (req, res) => {
         const { courseId } = req.params;
         const modules = modulesDao.findModulesForCourse(courseId);
         res.json(modules);
-    });
+    };
 
-    app.post("/api/courses/:courseId/modules", (req, res) => {
+    const createModuleForCourse = (req, res) => {
         const { courseId } = req.params;
         const module = {
             ...req.body,
@@ -34,7 +40,12 @@ export default function CourseRoutes(app) {
         };
         const newModule = modulesDao.createModule(module);
         res.send(newModule);
-    });
+    };
 
-
+    app.get("/api/courses", findAllCourses);
+    app.post("/api/courses", createCourse);
+    app.delete("/api/courses/:courseId", deleteCourse);
+    app.put("/api/courses/:courseId", updateCourse);
+    app.get("/api/courses/:courseId/modules", findModulesForCourse);
+    app.post("/api/courses/:courseId/modules", createModuleForCourse);
 }
